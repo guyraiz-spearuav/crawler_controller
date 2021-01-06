4261f34a95ae7cb7295a5e5141334b2895a1114e
Guy Raiz SpearUAV <guy@spearuav.com>
2021-01-06 15:30:18 +0200

    2 motor testing

--- crawler_conrtroller/crawler_conrtroller.ino
+++ crawler_conrtroller/crawler_conrtroller.ino
@@ -1,3 +1,15 @@
+typedef struct {
+  int in1;
+  int in2;
+  int en;
+} MOTOR;
+
+void motors_run(void);
+
+void motor_move(MOTOR, int, bool);
+int pwm_output (int);
+bool move_direction ( int);
+
 const int INPUT_PIN_FROM_FC [4] = {2, 3, 4, 5};
 const int REAR_RIGHT_IN1 = 7;
 const int REAR_RIGHT_IN2 = 8;
@@ -6,25 +18,27 @@ const int REAR_LEFT_IN1 = 10;
 const int REAR_LEFT_IN2 = 11;
 const int REAR_LEFT_EN = 12;
 
-typedef struct{
-  int in1;
-  int in2;
-  int en;
-}MOTOR;
+const int rear_right = 0;
+const int rear_left = 1;
+const int fowards = 0;
+const int backwards = 1;
 
-MOTOR rear_right;
-MOTOR rear_left;
+
+MOTOR motor_position[4] = {
+  {REAR_RIGHT_IN1, REAR_RIGHT_IN2, REAR_RIGHT_EN},
+  {REAR_LEFT_IN1, REAR_LEFT_IN2, REAR_LEFT_EN},
+  {REAR_LEFT_IN1, REAR_LEFT_IN2, REAR_LEFT_EN},
+  {REAR_LEFT_IN1, REAR_LEFT_IN2, REAR_LEFT_EN},
+};
 
 int input_from_fc [4];
-void motor_stop(MOTOR motor);
-void motor_freewheel(MOTOR motor);
-void motor_forwards(MOTOR motor, int);
-void motor_backwards(MOTOR motor, int);
+int pwm_output_value [4];
+bool motor_direction[4];
+
 void setup() {
 }
 
 void loop() {
   readings();
-  motor_stop(rear_right);
-  motor_stop(rear_left);
+  motors_run();
 }
--- crawler_conrtroller/motors.ino
+++ crawler_conrtroller/motors.ino
@@ -1,23 +1,20 @@
-void motor_stop(MOTOR motor) {
-  digitalWrite(motor.in1, false);
-  digitalWrite(motor.in2, false);
-  digitalWrite(motor.en, true);
+void motors_run() {
+  for (int i = 0; i <= 3; i++) {
+    pwm_output_value[i] = pwm_output(input_from_fc[i]);
+    motor_direction[i] = move_direction(input_from_fc[i]);
+    motor_move(motor_position[i], pwm_output_value[i], motor_direction[i]);
+  }
 }
 
-void motor_freewheel(MOTOR motor) {
-  digitalWrite(motor.in1, false);
-  digitalWrite(motor.in2, false);
-  digitalWrite(motor.en, false);
+int pwm_output(int input) {
+  return abs(map(input, 1000, 2000, -255, 255));
+}
+bool move_direction(int input) {
+  return (input >= 1500);
 }
 
-void motor_forwards(MOTOR motor, int pwm_value) {
-  digitalWrite(motor.in1, false);
-  digitalWrite(motor.in2, true);
-  analogWrite(motor.en, pwm_value);
-}
-
-void motor_backwards(MOTOR motor, int pwm_value) {
-  digitalWrite(motor.in1, true);
-  digitalWrite(motor.in2, false);
+void motor_move(MOTOR motor, int pwm_value, bool direction) {
+  digitalWrite(motor.in1, direction);
+  digitalWrite(motor.in2, !direction);
   analogWrite(motor.en, pwm_value);
 }
